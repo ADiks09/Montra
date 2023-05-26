3b31d60cb872135f9c31837826838e34724302db
ADiks <antondikson@gmail.com>
2023-05-26 18:52:32 +0200

    [feature] - init project

---
+++ Montra/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+internal struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+private struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
